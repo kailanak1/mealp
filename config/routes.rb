02039112e717f53ecd2342f3a4a134ca062cb54d
kailanak1 <kailanakahawaii@gmail.com>
2020-03-04 15:49:33 -0800

    did more css

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   
+  get 'tags/:tag', to: 'posts#index', as: :tagged
   get '/login' => "sessions#new", as:"login" #new users having trouble staying logged in when they first sign up
   post '/sessions' => "sessions#create"
   get '/logout' => 'sessions#destroy'
