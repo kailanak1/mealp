6424c8ecdbd5a4b8f01caa66aac5c137358b097e
kailanak1 <kailanakahawaii@gmail.com>
2020-03-07 12:53:57 -0800

    working on friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,17 @@
 class FriendshipsController < ApplicationController
+
+    def index 
+        @friendship = current_user.friendships
+    end
+
+    def create 
+     
+        @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+        if @friendship.save 
+            flash[:messages] = "Added friend!"
+            redirect_to friendship_path(@friendship)
+        else 
+            flash[:messages] = "Unable to add friend :("
+        end
+    end
 end
