d8c5251808e02f0a4b8a8405491a271c2166ba56
kailanak1 <kailanakahawaii@gmail.com>
2020-03-07 15:30:38 -0800

    made friends visible on user profile page

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -9,9 +9,12 @@ class FriendshipsController < ApplicationController
         @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
         if @friendship.save 
             flash[:messages] = "Added friend!"
-            redirect_to friendship_path(@friendship)
+           
         else 
             flash[:messages] = "Unable to add friend :("
         end
     end
+
+
+    
 end
