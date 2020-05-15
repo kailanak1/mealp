a0bca395953bce5297afab9d0f2438259a1a40b8
kailana <kailanakahawaii@gmail.com>
2020-05-15 15:15:09 -0700

    changing to postgres

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,16 +5,25 @@ class FriendshipsController < ApplicationController
     end
 
     def create 
-     
+        @user = current_user 
         @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-        if @friendship.save 
-            flash[:messages] = "Added friend!"
-           
+        if !current_user.friends.exists?(@user.id)
+            @friendship.save 
+            flash[:messages] = "Added friend! :)"
+            redirect_to current_user 
         else 
             flash[:messages] = "Unable to add friend :("
         end
     end
 
 
-    
+    def destroy
+        @friendship = current_user.friendships.find(params[:id])
+        @friendship.destroy
+        flash[:notice] = "Removed friendship."
+        redirect_to current_user
+      end
+
+
+
 end
